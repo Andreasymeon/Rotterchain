27e621d3087b15783e06d4f2723dc495fa4a3e91
Glenn Carroy <glenncarroy@Glenns-MacBook-Pro.local>
2018-03-21 21:43:11 +0100

    new deal set

--- _header.php
+++ _header.php
@@ -1,7 +1,7 @@
 <header>
+	<h1><a href="index.php">Ingridt</a></h1>
 	<nav>
 		<ul>
-			<li><a href="index.php">Home</a></li>
 			<li><a href="contracts.php">Contracts</a></li>
 			<li><a href="userProfile.php">Profile</a></li>
 			<li id="newDealButton"><a href="newDealMaker.php">New deal</a></li>
--- newDealMaker.php
+++ newDealMaker.php
@@ -5,8 +5,35 @@
 
 	<?php include '_header.php' ?>
 	
-<main>
-	
+<main id="DealMakerMain">
+	<section id="buyerFormSection">
+		<h2>Buy</h2>
+
+<!--===== BACKEND =====-->
+
+		<form action=" " method="POST">
+			<input type="text" name="buyerSearch" placeholder="What are you looking for?">
+			<input class="button" type="submit" name="submit" value="Search">
+		</form>
+
+<!--===================-->
+
+	</section>
+	<section id="sellerFormSection">
+		<h2>Sell</h2>
+
+<!--===== BACKEND =====-->
+
+		<form action=" " method="POST">
+			<input type="text" name="sellerArticle" placeholder="What do you sell?">
+			<input type="text" name="sellerQuantity" placeholder="Which quantity?">
+			<input type="text" name="sellerPrice" placeholder="For which price?">
+			<input class="button" type="submit" name="submit" value="Post">
+		</form>
+
+<!--====================-->
+
+	</section>
 </main>
 <footer>
 	
