bea7ed454a9dd83d4dd4794ed3e5165b8fe2e331
Glenn Carroy <glenncarroy@Glenns-MacBook-Pro.local>
2018-03-21 19:33:39 +0100

    ajout base html

---
+++ _head.php
@@ -0,0 +1,5 @@
+<head>
+	<meta charset="UTF-8">
+	<title> INGridt</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
---
+++ _header.php
@@ -0,0 +1,10 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="contracts.php">Contracts</a></li>
+			<li><a href="userProfile.php">Profile</a></li>
+			<li id="newDealButton"><a href="newDealMaker.php">New deal</a></li>
+		</ul>
+	</nav>
+</header>
