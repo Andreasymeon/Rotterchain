85da84cfe46ed12b216d7cc567dae4181438223a
Glenn Carroy <glenncarroy@Glenns-MacBook-Pro.local>
2018-03-22 01:34:56 +0100

    dernière version du contrat vente

--- _head.php
+++ _head.php
@@ -3,4 +3,4 @@
 	<title> INGridt</title>
 	<link href="https://fonts.googleapis.com/css?family=Oxygen:300,400,700|Source+Sans+Pro:400,600" rel="stylesheet">
 	<link rel="stylesheet" href="assets/CSS/style.css">
-</head>
+</head>	
--- newDealMaker.php
+++ newDealMaker.php
@@ -28,6 +28,20 @@
 			<input type="text" name="sellerArticle" placeholder="What do you sell?">
 			<input type="text" name="sellerQuantity" placeholder="Which quantity?">
 			<input type="text" name="sellerPrice" placeholder="For which price?">
+			<div class="sellingCond">
+				<label for="insurance">Do you provide insurance?</label>
+					<select name="insurance">
+						<option value="Yes">Yes</option>
+						<option value="No">No</option>
+					</select>
+			</div>
+			<div class="sellingCond">
+				<label for="transportation">Do you provide transportation?</label>
+					<select name="insurance">
+						<option value="Yes">Yes</option>
+						<option value="No">No</option>
+					</select>
+			</div>
 			<input class="button" type="submit" name="submit" value="Post">
 		</form>
 
